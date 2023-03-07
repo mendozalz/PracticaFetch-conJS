8cb7d28e7a54b907d024fb323921e2767eeb4894
Lenin Jose Mendoza Espina <mendoza124302@gmail.com>
2023-03-07 13:37:07 -0500

    final de esta sección

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const obtn = document.querySelector(".contenido"); 
 
-function traerAPI(){
-    fetch('https://randomuser.me/api/')
+/* function traerAPI(){
+    fetch('https://www.garantiascomunitarias.com/wp-json/wp/v2/posts')
     .then(data => data.json())
     .then(data => {
-        obtn.innerHTML=`<img src="${data.results["0"].picture.large}" width="200px" class="img-fluid rounded-circle" alt="imagen de perfil">
+        obtn.innerHTML=`<img src="${data.content}" width="200px" class="img-fluid rounded-circle" alt="imagen de perfil">
         <p>
-        Email:${data.results["0"].email}<br>
-        Nombre: ${data.results["0"].name.first}<br>
-        Apellido: ${data.results["0"].name.last}<br>
-        Cell:${data.results["0"].cell}
+        imagen: ${data.content}
+       
         </p>`
         console.log(data)
     }) 
     
 
     
-}
+} */
 
+fetch("https://www.garantiascomunitarias.com/wp-json/wp/v2/posts")
+.then(res=>res.json())
+.then(content=>{console.log(content.rendered)})
